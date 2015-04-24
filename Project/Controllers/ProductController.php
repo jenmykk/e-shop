fde7e2c3a510dc8ae0d1226c415f1374545d97be
susanneengkvist <susanneengkvist@gmail.com>
2015-04-24 15:11:10 +0200

    Added function showAllProducts and function getById

--- Project/Controllers/ProductController.php
+++ Project/Controllers/ProductController.php
@@ -1,2 +1,20 @@
 <?php
-echo "This is the product controller.";
+namespace Project\Controllers;
+
+use Ionian\Core\Controller;
+use Ionian\Database\Database;
+
+class productController extends Controller{
+    public function showAllProductsAction(){
+        $stm = Database::get()->prepare("SELECT * FROM products");
+        $stm->execute();
+        $this->outputJSON($stm->fetchAll());
+    }
+    public function getByIdAction($id){
+        $stm = Database::get()->prepare("SELECT * FROM products WHERE product_id = :id");
+        $stm->bindParam(":id",$id);
+        $stm->execute();
+        $this->outputJSON($stm->fetch());
+    }
+    
+}
